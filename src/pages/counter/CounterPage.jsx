9e7964fc9ad5a9ad0f2a8d1d2685e94194d6461e
TareqMonwer <tareqmonwer137@gmail.com>
2021-09-17 16:24:18 +0600

    :zap: counter page design completed.

--- src/pages/counter/CounterPage.jsx
+++ src/pages/counter/CounterPage.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import Layout from '../../containers/Layout';
+import React, { useState } from 'react';
+import Layout from 'containers/Layout';
 
 const CounterPage = () => {
+  const [count, setCount] = useState(0);
+  const handleSetCount = () => setCount(prev => prev + 1)
+
   return (
     <Layout>
-      <h1>Count 0</h1>
-      <button>
-        Increment
-      </button>
+      <div className="flex flex-col justify-center items-center text-center">
+        <h1 className="text-3xl my-5 p-10 border border-gray-300 
+        rounded inline-block">
+          {count}
+        </h1>
+        <button
+          className="bg-indigo-700 text-white px-3 py-2 rounded"
+          onClick={handleSetCount}>
+          Button
+        </button>
+      </div>
     </Layout>
   );
 }
